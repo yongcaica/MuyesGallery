e7df7e6f1b7bb696bfd43fef5243ca893456657c
Yong Cai <36556902+yongcaica@users.noreply.github.com>
2018-11-12 15:39:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ var indexRoutes    = require("./routes/index"),
     artworkRoutes  = require("./routes/artworks")
 
 mongoose.connect("mongodb://127.0.0.1:27017/muyesgallery", {useNewUrlParser: true });
-// 连接成功
+// connect successfully
 mongoose.connection.on('open', function(){
     console.log('MongoDB Connection Successed');
 });
-// 连接失败
+// connect fail
 mongoose.connection.on('error', function(){
     console.log('MongoDB Connection Error');
 });
